ad7a41f0d726b2bc01d731979903cfc9f90f5e8b
erik <erik.seiert@meissa-gmbh.de>
2023-10-18 11:58:09 +0200

    Adapt restore to taiga

--- infrastructure/docker-backup/image/resources/restore.sh
+++ infrastructure/docker-backup/image/resources/restore.sh
@@ -12,19 +12,12 @@ function main() {
     file_env POSTGRES_USER
 
     # Restore latest snapshot into /var/backups/restore
-    rm -rf /var/backups/restore
     restore-directory '/var/backups/restore'
-
-    rm -rf /var/backups/gitea/*
-    rm -rf /var/backups/git/repositories/*
-    cp -r /var/backups/restore/gitea /var/backups/
-    cp -r /var/backups/restore/git/repositories /var/backups/git/
     
-    # adjust file permissions for the git user
-    chown -R 1000:1000 /var/backups
+    cp -r /var/backups/restore /media
 
-    # TODO: Regenerate Git Hooks? Do we need this?
-    #/usr/local/bin/gitea -c '/data/gitea/conf/app.ini' admin regenerate hooks
+    # adjust file permissions for the taiga user
+    chown -R taiga:taiga /media
 
     # Restore db
     drop-create-db
