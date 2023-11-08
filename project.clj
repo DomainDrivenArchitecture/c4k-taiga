77b385d0c2d3a627a3fa104f9ca81176851939dd
erik <erik.seiert@meissa-gmbh.de>
2023-11-08 15:37:29 +0100

    bump version to: 1.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/c4k-taiga "1.1.1"
+(defproject org.domaindrivenarchitecture/c4k-taiga "1.1.2-SNAPSHOT"
   :description "taiga c4k-installation package"
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
