13218b395ea3e3de6817392dc3232f6bc909b954
Mirco <mirco.zachmann@meissa.de>
2023-12-21 19:56:34 +0100

    Improvements docker image building

--- infrastructure/backup/image/resources/install.sh
+++ infrastructure/backup/image/resources/install.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-set -eux pipefail
+set -exo pipefail
 
-apt-get update > /dev/null;
+function main()
+{
+    {
+        install -m 0700 /tmp/entrypoint.sh /
+        install -m 0700 /tmp/entrypoint-start-and-wait.sh /
 
-install -m 0700 /tmp/entrypoint.sh /
-install -m 0700 /tmp/entrypoint-start-and-wait.sh /
+        install -m 0700 /tmp/init.sh /usr/local/bin/
+        install -m 0700 /tmp/backup.sh /usr/local/bin/
+        install -m 0700 /tmp/restore.sh /usr/local/bin/
+        install -m 0700 /tmp/restic-snapshots.sh /usr/local/bin/
+        
+        cleanupDocker
+    } > /dev/null
+}
 
-install -m 0700 /tmp/init.sh /usr/local/bin/
-install -m 0700 /tmp/backup.sh /usr/local/bin/
-install -m 0700 /tmp/restore.sh /usr/local/bin/
-install -m 0700 /tmp/restic-snapshots.sh /usr/local/bin/
+source /tmp/install_functions_debian.sh
+DEBIAN_FRONTEND=noninteractive DEBCONF_NOWARNINGS=yes main
