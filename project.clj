278b7350d99517bd45ee37af7826f74bebcda910
Michael Jerger <michael.jerger@meissa-gmbh.de>
2025-01-14 10:55:48 +0100

    update versions

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "https://domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [org.clojure/tools.reader "1.4.0"]                 
-                 [org.domaindrivenarchitecture/c4k-common-clj "6.1.3"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [org.clojure/tools.reader "1.5.0"]                 
+                 [org.domaindrivenarchitecture/c4k-common-clj "9.0.1"]
                  [hickory "0.7.1" :exclusions [viebel/codox-klipse-theme]]]
   :target-path "target/%s/"
   :source-paths ["src/main/cljc"
@@ -23,9 +23,9 @@
                        :main dda.c4k-taiga.uberjar
                        :uberjar-name "c4k-taiga-standalone.jar"
                        :dependencies [[org.clojure/tools.cli "1.1.230"]
-                                      [ch.qos.logback/logback-classic "1.5.0"
+                                      [ch.qos.logback/logback-classic "1.5.16"
                                        :exclusions [com.sun.mail/javax.mail]]
-                                      [org.slf4j/jcl-over-slf4j "2.0.12"]
+                                      [org.slf4j/jcl-over-slf4j "2.0.16"]
                                       [com.github.clj-easy/graal-build-time "1.0.5"]]}}
   :release-tasks [["test"]
                   ["vcs" "assert-committed"]
